8ff2ad1a683e26cecccf1063cc36dcdc70797b50
sowfrancis <sow.francis@gmail.com>
2016-12-06 10:40:21 +0100

    little fix on layouts

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -2,7 +2,7 @@ class SearchsController < ApplicationController
 
 	def index
 		@q = params[:q]
-		@jobs_tag = Job.name_tag(@q)
-		@jobs_place = Job.in_place_named(@q)
+		@jobs_tag = Job.name_tag(@q) #all job near position
+		@jobs_place = Job.in_place_named(@q) #job with tag
 	end
 end
