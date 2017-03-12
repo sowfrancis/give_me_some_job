e6e2b1154de037d65faa7bb5b7e73e477e3d64ce
sowfrancis <sow.francis@gmail.com>
2017-03-12 16:41:48 +0100

    change create candidacy method

--- app/controllers/candidacies_controller.rb
+++ app/controllers/candidacies_controller.rb
@@ -9,16 +9,20 @@ class CandidaciesController < ApplicationController
 	end
 
 	def create_multiple
-		tag = Tag.find_by(name: params[:tag_name]) if Tag.find_by(name: params[:tag_name]).present?
-		tag.jobs.each do |job|
-			@send_candidacy_service = SendCandidacy.new(job, current_user)
-			@candidacy = @send_candidacy_service.send_new_candidacy
-			if @candidacy.valid?
-				flash[:notice] = "vos candidatures multiples ont bien été envoyer!"
-			else
-				flash[:notice] = "vos candidatures multiples n'ont pas été envoyer,
-													veuillez rééssayer plus tard!"
+		tag = Tag.find_by(name: params[:tag_name])
+		if tag.present?
+			tag.jobs.each do |job|
+				@send_candidacy_service = SendCandidacy.new(job, current_user)
+				@candidacy = @send_candidacy_service.send_new_candidacy
+				if @candidacy.valid?
+					flash[:notice] = "vos candidatures multiples ont bien été envoyer!"
+				else
+					flash[:notice] = "vos candidatures multiples n'ont pas été envoyer,
+														veuillez rééssayer plus tard!"
+				end
 			end
+			else
+				flash[:notice] = "Il n'y a pas de job dans ce domaine. #{tag}"
 		end
 		redirect_to user_path(current_user)
 	end
